d1d89097c4def73b78ea94fa4a43eaf1c3a49315
Mikhail <a104s@mail.ru>
2022-05-28 23:03:29 +0300

    Homework

---
+++ Task41/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Write("Введите числа через пробел : ");
+int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int count = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums[i] > 0) count +=1;
+}
+Console.WriteLine($"Вы ввели {count} чисел, больших нуля");
---
+++ Task43/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите коэффициенты K и B первой функции через пробел :");
+float [] first = Console.ReadLine().Split().Select(float.Parse).ToArray();
+Console.Write("Введите коэффициенты K и B второй функции через пробел :");
+float[] sec = Console.ReadLine().Split().Select(float.Parse).ToArray();
+float x = (sec[1] - first[1])/(first[0]-sec[0]);
+float y  = first[0] * x + first[1];
+Console.WriteLine();
+Console.WriteLine($"x = {x}; y = {y}");
+
+
